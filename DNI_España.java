c6c0ee41bee95e153c769982b04119256c683eec
Juan Carlos Miranda Hinojosa <30374477+MirandaHinojosa@users.noreply.github.com>
2024-12-10 12:51:50 +0100

    carga DNI_ESPAÑA

---
+++ DNI_España.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.EjerciciosJAVA_DAM;
+
+/**
+ *
+ * @author jcmiranda
+ * 
+ * 
+ * Ejercicio: En España cada persona está identificada con un Documento Nacional de Identidad (DNI) en el que figura un número y una letra,
+ * por ejemplo 56999545W. La letra que sigue al número se calcula siguiendo la metodología que vamos a indicar
+ * 
+ */
+public class DNI_España {
+    public static void main (String [] args){
+        System.out.println("com.mycompany.EjerciciosJAVA_DAM.NewClass.main()");
+    }
+}
